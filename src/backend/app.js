de14a735c499ee08452dcd930bec3dc81c9b4137
Thuve Rakan <123587624+thuve-codes@users.noreply.github.com>
2025-03-15 12:51:12 +0530

    ADD : backend new files ADD : frontend new files DELETE : public/favicon.ico DELETE : public/index.html

--- src/backend/app.js
+++ src/backend/app.js
@@ -10,13 +10,15 @@ dot.config({path: path.join(__dirname,'config', 'config.env')});
 const products = require('./routes/product');   
 const orders = require('./routes/order');   
 
+const cors = require('cors');
+
 connectDatabase();
 
 app.use(express.json());
 
 app.use('/api/v1', products);
 app.use('/api/v1', orders);
-
+app.use(cors());
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${process.env.PORT} in ${process.env.NODE_ENV} mode`);
 });
--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Home from './pages/Home';
 
 function App() {
   return (
     <div className="App">
-    <h1>TEST</h1>
+      <Header />
+    <Home />
+    <Footer />
     </div>
   );
 }
